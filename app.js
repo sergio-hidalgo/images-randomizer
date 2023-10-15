42c930e796f6a35d0e67e382a8ffcbe513bd7f45
Sergio Hidalgo <65049319+sergio-hidalgo@users.noreply.github.com>
2023-10-15 19:10:51 +0200

    Add files via upload
    replace app.js and style.css files from original upload

--- app.js
+++ app.js
@@ -1,61 +1,42 @@
 const container = document.querySelector('.content');
+const dataPath = 'data/data.json';
 
-/* 
-const jsonObject = 'data/data.json';
+async function getData() {
+  try {
+    return fetch(dataPath)
+      .then((res) => res.json())
+      .then((data) => data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-function getData() {
-  const listImg = [];
+async function displayImages() {
+  const objectSet = await getData();
 
-  fetch(jsonObject)
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((element) => {
-        listImg.push(element.url);
-      });
+  const setObj = randomSetObjects(objectSet, 6);
+  const rows = 2;
+  const columns = 3;
 
-      console.log(listImg);
-    });
+  for (let i = 0; i < rows * columns; i++) {
+    const div = document.createElement('div');
+    div.className = 'imageWrapper';
 
-  return listImg;
-} 
+    const img = document.createElement('img');
+    img.src = `${setObj[i].url}`;
+    img.loading = 'lazy';
+    div.appendChild(img);
 
-const objectSet = getData();
+    const place = document.createElement('h3');
+    place.innerHTML = `${setObj[i].name}`;
+    div.appendChild(place);
 
-*/
+    const country = document.createElement('h2');
+    country.innerHTML = `${setObj[i].country}`;
+    div.appendChild(country);
 
-const objectSet = [
-  [1, 'images/img1.jpg', 'The Alps', 'Switzerland'],
-  [2, 'images/img2.jpg', 'Atacama desert', 'Chile'],
-  [3, 'images/img3.jpg', 'Bruges street', 'Belgium'],
-  [4, 'images/img4.jpg', 'Cape Town cliff', 'South Africa'],
-  [5, 'images/img5.jpg', 'Dubrovnik bay', 'Croatia'],
-  [6, 'images/img6.jpg', 'Marrakesh souk', 'Morocco'],
-  [7, 'images/img7.jpg', 'Koh Tao beach', 'Thailand'],
-  [8, 'images/img8.jpg', 'Kyoto palace', 'Japan'],
-  [9, 'images/img9.jpg', 'Mount Cook', 'New Zealand'],
-  [10, 'images/img10.jpg', 'Madrid Gran Via', 'Spain'],
-  [11, 'images/img11.jpg', 'Montevideo square', 'Uruguay'],
-  [12, 'images/img12.jpg', 'Hidden waterfall', 'Laos'],
-];
-
-const setObj = randomSetObjects(objectSet, 8);
-const rows = 2;
-const columns = 4;
-
-for (let i = 0; i < rows * columns; i++) {
-  const div = document.createElement('div');
-  div.className = 'imageWrapper';
-
-  const img = document.createElement('img');
-  img.src = `${setObj[i][1]}`;
-  img.loading = 'lazy';
-  div.appendChild(img);
-
-  const title = document.createElement('h2');
-  title.innerHTML = `${setObj[i][3]}`;
-  div.appendChild(title);
-
-  container.appendChild(div);
+    container.appendChild(div);
+  }
 }
 
 function randomSetObjects(set, n) {
@@ -65,3 +46,5 @@ function randomSetObjects(set, n) {
 
   return randomSet;
 }
+
+displayImages();
